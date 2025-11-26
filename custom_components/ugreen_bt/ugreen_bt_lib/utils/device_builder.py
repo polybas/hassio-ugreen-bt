acaf472195b49b10746c8a70f353d4ed35d23ba5
polybas <vetalostapenko@gmail.com>
2025-11-26 17:53:29 +0200

    Update device_builder.py

--- custom_components/ugreen_bt/ugreen_bt_lib/utils/device_builder.py
+++ custom_components/ugreen_bt/ugreen_bt_lib/utils/device_builder.py
@@ -6,16 +6,16 @@ from ..devices.gs600 import GS600
 from ..devices.gs1200 import GS1200
 
 DEVICE_NAME_RE = re.compile(
-    r"^(GS600)(\d+)$"
+    r"^gs\s?(\d+)$"
 )
 
 
 def build_device(address: str, name: str):
     match = DEVICE_NAME_RE.match(name)
-    if match[1] == "GS600":
-        return GS600(address, match[2])
-    if match[1] == "GS1200":
-        return GS1200(address, match[2])
+    if match[1] == "600":
+        return GS600(address, "GS 600")
+    if match[1] == "1200":
+        return GS1200(address, "GS 1200")
    
 
 
